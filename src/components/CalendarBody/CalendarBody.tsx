a108c0f3fefbe01abcc793b7d885a3f3cc06c75b
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2023-02-08 23:21:56 +0900

    [ADD]버튼 클릭 추가

--- src/components/CalendarBody/CalendarBody.tsx
+++ src/components/CalendarBody/CalendarBody.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 
 const WEEKS = ['일', '월', '화', '수', '목', '금', '토'];
 
 const CalendarBody = () => {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  const clickDateHandler = (date: Date | '') => {
+    if (date === '') {
+      return;
+    }
+    if (startDate === null && endDate === null) {
+      setStartDate(date);
+    } else if (
+      !!startDate &&
+      endDate === null &&
+      (startDate.getTime() === date.getTime() || startDate.getTime() >= date.getTime())
+    ) {
+      setStartDate(null);
+    } else if (!!startDate && endDate === null) {
+      setEndDate(date);
+    } else if (!!startDate && !!endDate) {
+      setStartDate(null);
+      setEndDate(null);
+    }
+  };
+
   const today = new Date();
   const firstDay = new Date(today.getFullYear(), today.getMonth(), 1);
   const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
@@ -22,32 +45,84 @@ const CalendarBody = () => {
     <CalBody>
       <Days>
         {WEEKS.map((el, idx) => (
-          <DayButton key={idx}>{el}</DayButton>
+          <DayButton key={idx} selected={false}>
+            {el}
+          </DayButton>
         ))}
       </Days>
       <FirstWeeks>
-        {totalDateArr.slice(0, 7).map(el => (
-          <DayButton>{el === '' ? '' : el.getDate()}</DayButton>
+        {totalDateArr.slice(0, 7).map((el, idx) => (
+          <DayButton
+            key={idx}
+            selected={
+              !!el
+                ? el?.getTime() === startDate?.getTime() || el?.getTime() === endDate?.getTime()
+                : false
+            }
+            onClick={() => clickDateHandler(el)}
+          >
+            {el === '' ? '' : el.getDate()}
+          </DayButton>
         ))}
       </FirstWeeks>
       <SecondWeeks>
-        {totalDateArr.slice(7, 14).map(el => (
-          <DayButton>{el === '' ? '' : el.getDate()}</DayButton>
+        {totalDateArr.slice(7, 14).map((el, idx) => (
+          <DayButton
+            key={idx}
+            selected={
+              !!el
+                ? el?.getTime() === startDate?.getTime() || el?.getTime() === endDate?.getTime()
+                : false
+            }
+            onClick={() => clickDateHandler(el)}
+          >
+            {el === '' ? '' : el.getDate()}
+          </DayButton>
         ))}
       </SecondWeeks>
       <ThirdWeeks>
-        {totalDateArr.slice(14, 21).map(el => (
-          <DayButton>{el === '' ? '' : el.getDate()}</DayButton>
+        {totalDateArr.slice(14, 21).map((el, idx) => (
+          <DayButton
+            key={idx}
+            selected={
+              !!el
+                ? el?.getTime() === startDate?.getTime() || el?.getTime() === endDate?.getTime()
+                : false
+            }
+            onClick={() => clickDateHandler(el)}
+          >
+            {el === '' ? '' : el.getDate()}
+          </DayButton>
         ))}
       </ThirdWeeks>
       <FourthWeeks>
-        {totalDateArr.slice(21, 28).map(el => (
-          <DayButton>{el === '' ? '' : el.getDate()}</DayButton>
+        {totalDateArr.slice(21, 28).map((el, idx) => (
+          <DayButton
+            key={idx}
+            selected={
+              !!el
+                ? el?.getTime() === startDate?.getTime() || el?.getTime() === endDate?.getTime()
+                : false
+            }
+            onClick={() => clickDateHandler(el)}
+          >
+            {el === '' ? '' : el.getDate()}
+          </DayButton>
         ))}
       </FourthWeeks>
       <FifthWeeks>
-        {totalDateArr.slice(28, 35).map(el => (
-          <DayButton>{el === '' ? '' : el.getDate()}</DayButton>
+        {totalDateArr.slice(28, 35).map((el, idx) => (
+          <DayButton
+            key={idx}
+            selected={
+              !!el
+                ? el?.getTime() === startDate?.getTime() || el?.getTime() === endDate?.getTime()
+                : false
+            }
+            onClick={() => clickDateHandler(el)}
+          >
+            {el === '' ? '' : el.getDate()}
+          </DayButton>
         ))}
       </FifthWeeks>
       <SixthWeeks></SixthWeeks>
@@ -62,7 +137,6 @@ const CalBody = styled.main`
   flex-direction: column;
   flex: 7;
   width: 100%;
-  background-color: aqua;
 `;
 
 const Days = styled.div`
@@ -71,13 +145,39 @@ const Days = styled.div`
   justify-content: space-evenly;
   align-items: center;
   width: 100%;
+  background-color: bisque;
 `;
 
-const DayButton = styled.div`
+const DayButton = styled.div<{ selected: boolean }>`
+  flex: 1;
+  position: relative;
   width: 40px;
   height: 40px;
   font-weight: 500;
   text-align: center;
+  line-height: 40px;
+  z-index: 10;
+  ${({ selected }) => {
+    console.log(selected);
+    if (selected) {
+      return css`
+        ::before {
+          content: '';
+          display: block;
+          position: absolute;
+          left: 50%;
+          transform: translateX(-50%);
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          background-color: beige;
+          z-index: -2;
+        }
+        /* border-radius: 50%; */
+        /* background-color: bisque; */
+      `;
+    }
+  }}
 `;
 
 const FirstWeeks = styled.div`
@@ -86,7 +186,6 @@ const FirstWeeks = styled.div`
   justify-content: space-evenly;
   align-items: center;
   width: 100%;
-  background-color: aliceblue;
 `;
 
 const SecondWeeks = styled(FirstWeeks)``;
